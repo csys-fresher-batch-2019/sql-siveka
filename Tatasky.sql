dc79fb2e3c3ab88fb92ae6416dd0ebc50d35487d
siveka <sivekas@gmail.com>
2019-12-29 22:09:23 +0530

    Update Tatasky.sql

--- Tatasky.sql
+++ Tatasky.sql
@@ -4,8 +4,8 @@ channel_name varchar2(40) not null,
 channel_id number(10) not null,
 language varchar2(25) not null,
 free_pay char(5) not null,
-price number (10) not null,
-price_with_tax number (10) not null,
+price decimal(4,2) not null,
+price_with_tax decimal (4,2) not null,
 constraint channel_id_pk primary key (channel_id)
 );
 
@@ -25,9 +25,8 @@ create table selection_process
 selection_id number(5) not null,
 channel_id number(10) not null,
 user_id number(5)not null,
-price_with_tax number(10) not null,
-payment_status char(25) default Inprogress,
-primary key (selection_id),
+price_with_tax decimal(4,2) not null,
+payment_status char(25) default 'Inprogress',
 foreign key (channel_id) references channel_list (channel_id),
 foreign key (user_id) references user_list (user_id)
 );
@@ -36,10 +35,10 @@ create table selected_list
 (
 selection_id number(5) not null,
 user_id number(5) not null,
-total_amount number (10) not null,
+total_amount decimal (5,2)not null,
 active_date timestamp default systimestamp,
 deactive_date date,
-payment_status char(15) default Completed,
+payment_status char(15) default 'Completed',
 comments varchar2 (100),
 foreign key (selection_id) references selection_process(selection_id)
 );
@@ -57,9 +56,8 @@ select * from user_list;
 
 create sequence selection_process_seq start with 1001 increment by 1;
 
-insert into selection_process (selection_id,channel_id,user_id,price_with_tax)
-values (1001,(select channel_id from channel_list where channel_id=&channel_id),
-(select user_id from user_list where user_id=&user_id),(select price_with_tax from channel_list where channel_id=channel_id));
+insert into selection_process (selection_id,user_id,channel_id,price_with_tax)
+values (&selection_id,&user_id,&channel_id,(select price_with_tax from channel_list where channel_id=&channel_id));
 
 select * from selection_process;
 
@@ -71,5 +69,5 @@ select * from selection_process;
 drop SEQUENCE selection_process_seq;
 
 insert into selected_list (selection_id,user_id,total_amount,deactive_date)
-values (&selection_id,(select user_id from selection_process where selection_id=selection_id),(select sum(price_with_tax)from
-selection_process where user_id=user_id),(sysdate+3));
+values (&selection_id,(select user_id from selection_process where selection_id=&selection_id),(select sum(price_with_tax)from
+selection_process where user_id=&user_id),(sysdate+3));
